7f7ee624929bbad8539850e45674d7f4ad582d1c
Henrique Borges dos Santos <henrique.10agr@gamil.com>
2023-07-02 21:30:16 -0300

    Auditoria

--- server/database.sql
+++ server/database.sql
@@ -20,3 +20,48 @@ CREATE TABLE concept (
     study_list_id INTEGER NOT NULL,
     FOREIGN KEY (study_list_id) REFERENCES study_list(id)
 );
+
+CREATE TABLE audit (
+    id SERIAL PRIMARY KEY,
+    affected_table TEXT,
+    operation TEXT,
+    date_time TIMESTAMP DEFAULT NOW(),
+    previous_data JSONB,
+    current_data JSONB
+);
+
+
+CREATE OR REPLACE FUNCTION audit_trigger_function()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF (TG_OP = 'INSERT') THEN
+        INSERT INTO audit (affected_table, operation, current_data)
+        VALUES (TG_TABLE_NAME, 'INSERT', row_to_json(NEW));
+        RETURN NEW;
+    ELSIF (TG_OP = 'UPDATE') THEN
+        INSERT INTO audit (affected_table, operation, previous_data, current_data)
+        VALUES (TG_TABLE_NAME, 'UPDATE', row_to_json(OLD), row_to_json(NEW));
+        RETURN NEW;
+    ELSIF (TG_OP = 'DELETE') THEN
+        INSERT INTO audit (affected_table, operation, previous_data)
+        VALUES (TG_TABLE_NAME, 'DELETE', row_to_json(OLD));
+        RETURN OLD;
+    END IF;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER audit_trigger
+AFTER INSERT OR UPDATE OR DELETE ON client
+FOR EACH ROW
+EXECUTE FUNCTION audit_trigger_function();
+
+CREATE TRIGGER audit_trigger
+AFTER INSERT OR UPDATE OR DELETE ON study_list
+FOR EACH ROW
+EXECUTE FUNCTION audit_trigger_function();
+
+CREATE TRIGGER audit_trigger
+AFTER INSERT OR UPDATE OR DELETE ON concept
+FOR EACH ROW
+EXECUTE FUNCTION audit_trigger_function();
